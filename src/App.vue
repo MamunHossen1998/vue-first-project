a5cea505329047f62113bc124c46293677f674e7
MamunHossen1998 <50303005+MamunHossen1998@users.noreply.github.com>
2025-01-04 00:28:19 +0600

    custom event

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
     <description></description>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias tenetur ipsam, et qui ullam tempore dignissimos ab odit quis adipisci iure architecto fugiat unde iusto aliquid impedit sapiente repudiandae. Facere.</p>
     <allFriend :friends="friends"></allFriend>
-    <OnlineFriend :friends="friends"></OnlineFriend>
+    <OnlineFriend :friends="friends" @delete="deleteFriend"></OnlineFriend>
   </div>
 
 </template>
@@ -38,6 +38,14 @@ export default {
           {name:'D',online:false},
           {name:'E',online:true},
         ]
+    }
+  },
+  methods:{
+    deleteFriend(payload){
+      console.log(payload);
+      this.friends = this.friends.filter(friend =>{
+        return friend.name != payload.name;
+      })
     }
   }
 }
--- src/components/OnlineFriend.vue
+++ src/components/OnlineFriend.vue
@@ -2,10 +2,10 @@
     <div id="onlineFrien">
         <h2>Online Friend</h2>
         <div v-for="(friend,index) in friends" :key="index">
-            <h2 v-if="friend.online">{{friend.name}}</h2>
+            <h2 v-if="friend.online" @click="unfriend(friend.name)">{{friend.name}}</h2>
         </div>
     </div>
-</template>friends
+</template>
 <script>
 export default {
    name:'OnlineFriend',
@@ -13,6 +13,11 @@ export default {
    data(){
     return{
     }
+   },
+   methods:{
+        unfriend(name){
+            this.$emit('delete',{name})
+        }
    }
 }
 </script>
